874c9fbf6cd7381a01582264b4bfb4c08515d545
Šarūnas <107551364+sBACKcEND@users.noreply.github.com>
2022-10-06 09:11:04 +0300

    Add files via upload

---
+++ database/migrations/2022_10_05_073751_add_cars_and_owners_relation.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cars', function (Blueprint $table){
+            $table->foreign('owner_id')->references('id')->on('owners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
