fe5f130b4a19fe997c80f3f074700007e6f745fb
Šarūnas <107551364+sBACKcEND@users.noreply.github.com>
2022-10-10 06:14:17 +0300

    Upload files

---
+++ database/migrations/2022_10_06_101458_add_image_car.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table){
+            $table->unsignedBigInteger('car_id')->nullable()->default(null);
+            $table->foreign('car_id')->references('id')->on('cars');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
